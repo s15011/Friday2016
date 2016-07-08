c230737fbe436b02710630ea303268db0092e513
s15011 <s15011@std.it-college.ac.jp>
2016-07-08 10:44:06 +0900

    修正

--- training/t1001.py
+++ training/t1001.py
@@ -1,12 +1,12 @@
 # 与えられた2つのパラメータの合計を2倍したものが60をこえているかどうか
 def check_sum_2times_over_60(par1, par2):
     if (par1 + par2) * 2 > 60:
-        return print("true")
+        return
 
 # 与えられた金額に消費税率8%を含めた値が5000を超えているかどうか
 def tax_include(cost):
     if (cost * 1.08 > 5000):
-        return print("true")
+        return
 
 # 与えられたスコアを80以上なら'A'60以上80未満なら'B'、45以上60未満なら'C',
 # 45'未満'は'F'と返す
@@ -27,7 +27,6 @@ def factorial(num):
     for i in range(num):
         tmp *= i + 1
     print(tmp)
-
 
 # 与えられた数値の2の累乗を返す= 再起使用禁止。便利な演算子利用禁止。
 def power_of_two(num):
